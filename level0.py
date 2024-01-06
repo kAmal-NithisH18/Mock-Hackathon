c4c8fec28eb4238b595f981cf3fbb5b5c400783f
kAmal-NithisH18 <kamalshobi2004@gmail.com>
2024-01-06 10:09:04 +0530

    Graph nodes started

--- level0.py
+++ level0.py
@@ -1,6 +1,6 @@
 #"Level 0"
 import pandas as pd
-import json
+import networkx as nx
 
 
 dataset = r"Mock-Hackathon\Input data\level0.json"
@@ -10,15 +10,35 @@ neighbourhoods_df = pd.DataFrame(data['neighbourhoods'])
 restaurants_df = pd.DataFrame(data['restaurants'])
 
 neighbourhoods_df.dropna(inplace=True)
+restaurants_df.dropna(inplace=True)
 
 neighbours = []
+restaurant_distances = []
 
 for index, row in neighbourhoods_df.iterrows():
     neighbours.append(row['neighbourhoods']['distances'])
+for index, row in restaurants_df.iterrows():
+    restaurant_distances.extend(row['restaurants']['neighbourhood_distance'])
+
+print(restaurant_distances)
+
+#naming for mapping it into list
 names = ['n' + str(i) for i in range(len(neighbours))]
-print(names) 
 
+G = nx.Graph()
+num_houses = len(neighbours)
+G.add_nodes_from(range(num_houses))
 
+restaurant_node = 'restaurant'
+G.add_node(restaurant_node)
+
+# Connect the restaurant node to all neighborhood house nodes
+"""for i in range(num_houses):
+    G.add_edge(restaurant_node, i, weight=restaurant_distances[i])"""
+
+for i in range(num_houses):
+    for j in range(i + 1, num_houses):
+        G.add_edge(i, j, weight=neighbours[i][j])
 
 
 
